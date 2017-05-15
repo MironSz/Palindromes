187bcc93d0f0521ec9e4ac5bdcc8627faa22c97b
Miron Szewczyk <miron.szewczyk@autonomik.pl>
2017-05-15 11:57:42 +0200

    More functional, Solution integrated with Tests

--- Solution.scala
+++ Solution.scala
@@ -3,43 +3,106 @@ import math.min
 
 object Solution{
   def solution(s :String):Int={
-      val tb="#"+s+"&*"
-      val tablica:Array[Int]=new Array[Int](tb.length)
-      var j=0
-      for(i <- 1 until tb.length)
-        tablica(i)=0
+      val tb="*"+s+"#"
       def symetry(a:Int, b:Int):Int=b+b-a
-      def manacherRec(ob:Int, r:Int, par:Int, prev:Int, res:Int):Int={
+      def manacherRec(ob:Int, r:Int, par:Int, prev:Int, res:Int,palLength:Array[Int]):Int={
+        val resLocal=res + (prev until min(ob,tb.length)).foldLeft(0)((x:Int, y:Int)
+          =>if(x>=1e8||x<0) -1 else x+palLength(y))
 
-        var resLocal=res
-
-        for(i<- prev until min(ob,tb.length))
-          if(i<tb.length)
-            resLocal=resLocal+tablica(i)
         if(resLocal>1e8||resLocal<0)
           return -100000000
 
         if(ob+r+1-par>=tb.length)
           return resLocal
         if(tb(ob+r+1-par)==tb(ob-r-1))
-          return manacherRec(ob,r+1,par,ob,resLocal);
+          return manacherRec(ob,r+1,par,ob,resLocal,palLength);
 
-        tablica(ob)=r
-        j=ob-1
-        if(r>1)
-          while(j>=ob-r-1){
-            tablica(symetry(j,ob))=min  (tablica(j),   j-(ob-r));
-            if(j-tablica(j)-1<=ob-r)
-              return manacherRec( symetry(j,ob),  tablica(symetry(j,ob)), par , ob , resLocal )
-            j-=1
-          }
-          return manacherRec(ob+max(tablica(ob)-par,1),0,par,ob,resLocal)
+        palLength(ob)=r
+        def RecIter(j:Int,palLength:Array[Int]):Int={
+          palLength(symetry(j,ob))=min  (palLength(j),   j-(ob-r));
+          if(j-palLength(j)-1<=ob-r)
+            return manacherRec( symetry(j,ob),  palLength(symetry(j,ob)), par , ob , resLocal,palLength )
+          else
+            return RecIter(j-1,palLength)
         }
-      var res=manacherRec(1,0,0,0,0);
-      for(i<-1 until tb.length)
-        tablica(i)=0;
-      res=manacherRec(1,0,1,0,res)
+        if(r>1) return  RecIter(ob-1,palLength)
+        else   return manacherRec(ob+max(palLength(ob)-par,1),0,par,ob,resLocal,palLength)
+      }
+      var res=manacherRec(1,0,0,0,0,Array.fill(tb.length)(0));
+      res=manacherRec(1,0,1,0,res,Array.fill(tb.length)(0))
       if(res<0) res=(-1)
       return res;
     }
+//////////////////////////////////////////
+    def Generator1(length:Int,word1:String,word2:String):String=
+    {
+      def Rec(a:String,b:String):String=if(a.length+b.length>=length) a+b else Rec(a+b,a)
+      Rec(word1,word2)
+    }
+    def Generator2(length:Int,word1:String,word2:String):String=
+    {
+      def Rec(a:String,b:String):String=if(a.length+b.length>=length) a+b else Rec(a+b+a,b+a+b)
+      Rec(word1,word2)
+    }
+    def OnlyA(n:Int):String={
+      def Rec(m: Int,  sum: StringBuilder): String =
+      {
+        if(m==n) sum.toString
+        else Rec(m+1, sum.append('a'))
+      }
+       Rec(0,new StringBuilder )
+    }
+
+    def check():Boolean={
+
+      def checkS(s:String):Boolean={
+        if(solution(s)==brutForceSolution(s))
+          return true
+        else
+        {
+          println("expected "+brutForceSolution(s)+" but result is "+solution(s))
+          return false
+        }
+      }
+      var res=true
+      res&=checkS(OnlyA(10000))
+      res&=checkS(Generator1(100,"abba","bbbabbb"))
+      res&=checkS(Generator2(100,"abba","bbbabbb"))
+      res&=checkS("a")
+      res&=checkS("mmiirroonn")
+      res&=checkS("miron")
+      res&=checkS("torot")
+      res&=checkS("toror")
+      res&=checkS("aa")
+      res&=checkS("ab")
+      res&=checkS("")
+      return res
+
+    }
+    def main(args: Array[String]): Unit = {
+      if(check())
+        println("ok")
+      else
+        println("fail")
+      }
+      def brutForceSolution(s:String):Int={
+        var res=0
+
+        val tb="%"+s+"&"
+        for(i<-1 until tb.length-1)
+        {
+          for(par<-0 to 1)
+          {
+            var j=0
+            while(tb(i+j+1-par)==tb(i-(j+1)))
+              j+=1
+            res+=j
+          }
+        }
+        if(res<=1e8)
+          return res
+        return -1
+      }
+
+
 }
